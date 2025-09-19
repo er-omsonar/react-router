90f4cd190eeda16b991df0436861d6518a7f7de3
er-omsonar <er.omsonar@gmail.com>
2025-09-19 17:09:29 +0530

    using github api acees real time followers and profile picture

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -40,6 +41,7 @@ const router = createBrowserRouter(
      <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
       <Route path='user/:userid' element={<User/>}/>
+       <Route path='github' element={<Github/>}/>
    </Route>
   )
 )
