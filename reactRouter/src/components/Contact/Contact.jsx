85e0b08f52f5f8b01b95e34f8be2ad67d865e4bc
er-omsonar <er.omsonar@gmail.com>
2025-09-19 17:43:53 +0530

    deploy

--- reactRouter/src/components/Contact/Contact.jsx
+++ reactRouter/src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
                     <div className="grid grid-cols-1 md:grid-cols-2">
                         <div className="p-6 mr-2 bg-gray-100 sm:rounded-lg">
                             <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
-                                Get in touch: 
+                                Get in touch:
                             </h1>
                             <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
                                 Fill in the form to start a conversation
